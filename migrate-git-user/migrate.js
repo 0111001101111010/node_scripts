ac664583ffcb3cf5747b22f7a59a68643267e896
Stanley Zheng <dev.stanzheng@gmail.com>
2014-12-23 11:53:18 -0500

    get yourself

--- migrate-git-user/migrate.js
+++ migrate-git-user/migrate.js
@@ -2,7 +2,17 @@ var github = require("octonode");
 var user = process.env.GITHUB_USER;
 var pass = process.env.GITHUB_PASS
 
+
 var client = github.client({
   username: user,
   password: pass
+});
+
+
+//Person of interest
+
+var client = github.client();
+
+client.get('/users/stanzheng/repos', {}, function(err, status, body, headers) {
+  console.log(body); //json object
 });
