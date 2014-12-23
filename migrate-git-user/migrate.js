2a8d9704eea6d239679178ed312c40e4386521f5
Stanley Zheng <dev.stanzheng@gmail.com>
2014-12-23 17:08:50 -0500

    output function

--- migrate-git-user/migrate.js
+++ migrate-git-user/migrate.js
@@ -1,18 +1,39 @@
 var github = require("octonode");
 var user = process.env.GITHUB_USER;
 var pass = process.env.GITHUB_PASS
-
+var fs = require('fs');
 
 var client = github.client({
   username: user,
   password: pass
 });
 
+var ghme = client.me();
 
 //Person of interest
 
 var client = github.client();
 
 client.get('/users/stanzheng/repos', {}, function(err, status, body, headers) {
-  console.log(body); //json object
+  //console.log(body); //json object
+  //output(JSON.stringify(body));
 });
+
+
+//I AM
+ghme.info(function(err, data, headers) {
+  console.log("error: " + err);
+  //console.log("data: " + data);
+  //output(JSON.stringify(data));
+  // data.forEach(function(val, key) {
+  //   console.log(key)
+  // });
+  console.log("headers:" + headers);
+});
+
+function output(data) {
+  fs.writeFile('output.json', data, function(err) {
+    if (err) throw err;
+    console.log('It\'s saved!');
+  });
+}
