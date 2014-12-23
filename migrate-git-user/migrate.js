274a8971c242872c82669a32073157772f3a7740
Stanley Zheng <dev.stanzheng@gmail.com>
2014-12-23 11:46:22 -0500

    using octonode better documentation

--- migrate-git-user/migrate.js
+++ migrate-git-user/migrate.js
@@ -1,24 +1,8 @@
-var GitHubApi = require("github");
+var github = require("octonode");
 var user = process.env.GITHUB_USER;
 var pass = process.env.GITHUB_PASS
 
-var github = new GitHubApi({
-  // required
-  version: "3.0.0",
-  // optional
-  timeout: 5000
-});
-
-
-
-github.authenticate({
-  type: "basic",
+var client = github.client({
   username: user,
-  password: pass,
-});
-github.user.update({
-  location: "Space"
-}, function(err) {
-  console.log(err)
-  console.log("done!");
+  password: pass
 });
